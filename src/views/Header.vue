920a417621b854c88367aac9a353143cd3f63263
Nooptr <nooptr@techap.xyz>
2019-11-16 10:14:45 +0900

    buefy css導入し、Headerコンポーネントを追加

---
+++ src/views/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+        >
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item :to="{ path: '/' }">
+        Home
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
