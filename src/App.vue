133d12a83608c1d17c7dd23c09397a81372ba829
Nooptr <nooptr@techap.xyz>
2019-11-16 11:14:44 +0900

    Bodyのcssを修正

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,78 @@
   <div id="app">
     <div class="container">
       <Header />
-      <div class="notification">
-        This container is <strong>fluid</strong>: it will have a 32px gap on either side, on any
-        viewport size.
+      <div class="columns m-t-10 is-centered is-mobile">
+        <div class="column is-half">
+          <b-field grouped>
+            <b-input placeholder="Search..." expanded></b-input>
+            <p class="control">
+              <button class="button is-primary">Search</button>
+            </p>
+          </b-field>
+        </div>
+      </div>
+      <div class="columns is-centered">
+        <div>
+          <strong>Sharing a few of our favourite movies</strong>
+        </div>
+      </div>
+      <div class="columns is-centered">
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-5by4">
+                <img src="https://source.unsplash.com/random/1280x480">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content is-centered">
+                <p class="title is-6">Joker(2019)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-5by4">
+                <img src="https://source.unsplash.com/random/1280x480">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content is-centered">
+                <p class="title is-6">Joker(2019)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-5by4">
+                <img src="https://source.unsplash.com/random/1280x480">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content is-centered">
+                <p class="title is-6">Joker(2019)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-5by4">
+                <img src="https://source.unsplash.com/random/1280x480">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content is-centered">
+                <p class="title is-6">Joker(2019)</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -20,3 +89,20 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+$spaces: (0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50); // 0から50pxまでを用意
+$sides: (top, bottom, left, right); // 方向性
+
+@each $space in $spaces {
+  @each $side in $sides {
+    .m-#{str-slice($side, 0, 1)}-#{$space} {
+      margin-#{$side}: #{$space}px !important;
+    }
+
+    .p-#{str-slice($side, 0, 1)}-#{$space} {
+      padding-#{$side}: #{$space}px !important;
+    }
+  }
+}
+</style>
--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar>
+  <b-navbar type="is-light" spaced="true">
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <img
